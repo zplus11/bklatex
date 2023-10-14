5f287e1769b5ae9b755555b79c265bac0c1818ff
Naman Taggar <namantaggar.11@gmail.com>
2023-10-14 20:34:42 +0530

    Add files in new branch

--- functions.py
+++ functions.py
@@ -2,11 +2,15 @@ import pandas as pd
 import json
 import os
 
-
-
 def read_journals(path):
+    while not os.path.exists(path):
+        path = input("That file does not exist. Try again: ")
     with open(path, "r") as file:
         return json.load(file)
+
+def write_journals(path, entries):
+    with open(path, "w") as file:
+        json.dump(entries, file)
 
 def create_database(filename):
     if not os.path.exists(filename):
@@ -17,3 +21,47 @@ def create_database(filename):
         print(f"Made entries database with {filename}_entries.json name.")
     else:
         print("File with that name exists. Supply unique name or delete that file.")
+
+def add_entry(file):
+    add_date = input("Enter date of transaction: ")
+    add_debit = input("Enter debited account names. Separate with comma if more than one: ")
+    add_credit = input("Enter credited account names. Separate with comma if more than one: ")
+    add_debit_amt = input("Add debited amounts in same order of accounts. Separate with comma: ")
+    add_credit_amt = input("Add credited amounts in same order of accounts. Separate with comma: ")
+    add_debit_folio = input("Add folio numbers for debit accounts. Enter none if NA. Separate with comma: ")
+    add_credit_folio = input("Add folio numbers for credit accounts. Enter none if NA. Separate with comma: ")
+    add_narration = input("Add narration: ")
+    debit_accounts = [element.strip() for element in add_debit.split(",")]
+    credit_accounts = [element.strip() for element in add_credit.split(",")]
+    debit_amounts = [element.strip() for element in add_debit_amt.split(",")]
+    credit_amounts = [element.strip() for element in add_credit_amt.split(",")]
+    debit_folios = [element.strip() for element in add_debit_folio.split(",")]
+    credit_folios = [element.strip() for element in add_credit_folio.split(",")]
+    journals_data = read_journals(file)
+    entry = [add_date, debit_accounts, credit_accounts, debit_amounts, credit_amounts, debit_folios, credit_folios, add_narration]
+    journals_data["entries"].append(entry)
+    print("The following entry was added:")
+    print(f"{add_date}\t{debit_accounts}\t\t\t{debit_folios}\t{debit_amounts}\n\t\t\t{credit_accounts}\t\t\t{credit_folios}\t{credit_amounts}\n{add_narration}")
+    write_journals(file, journals_data)
+
+def print_journals(file_name, path):
+    journals_data = read_journals(path)
+    print(journals_data)
+    journal_commands = "%%%%%%%%%%%%%%%%%%%%%%\n% JOURNAL ENTRIES\n%%%%%%%%%%%%%%%%%%%%%%\n\n\journal{\n\n"
+    for entry in journals_data["entries"]:
+        print(entry)
+        if len(entry) == 1:
+            journal_commands = journal_commands + f"\t\jyear{{{entry[0]}}}\n\n"
+        else:
+            journal_commands = journal_commands + f"\t\jdr{{{entry[0].title()}}}{{{entry[1][0].title()}}}{{{entry[5][0]}}}{{{entry[3][0]}}}\n"
+            if len(entry[1]) > 1:
+                for i in range(1, len(entry[1])):
+                    journal_commands = journal_commands + f"\t\jdr{{}}{{{entry[1][i].title()}}}{{{entry[5][i]}}}{{{entry[3][i]}}}\n"
+            journal_commands = journal_commands + f"\t\jcr{{{entry[2][0].title()}}}{{{entry[6][0]}}}{{{entry[4][0]}}}\n"
+            if len(entry[2]) > 1:
+                for i in range(1, len(entry[2])):
+                    journal_commands = journal_commands + f"\t\jcr{{{entry[2][i].title()}}}{{{entry[6][i]}}}{{{entry[4][i]}}}\n"
+            journal_commands = journal_commands + f"\t\jnar{{{entry[7]}}}\n\n"
+    journal_commands = journal_commands + "}"
+    with open(f"{file_name}_journal.tex", "w") as file:
+        file.write(journal_commands)
--- processor.py
+++ processor.py
@@ -1,14 +1,30 @@
 from functions import *
 
-ch = input("Supply path of the entries file. Enter ""new"" to create a new database.\n")
-if ch.lower().strip() == "new":
+jpath = input("Supply path of the entries file. Enter ""new"" to create a new database.\n")
+
+if jpath.lower().strip() == "new":
     name = input("Enter (unique) name for entries file: ")
     create_database(name)
+    name = name + "_entries.json"
+    jpath = name
 else:
-    if not os.path.exists(ch):
-        print("That file does not exist.")
-        sleep(3)
+    journals_data = read_journals(jpath)
+    print(f"Loaded the data from given path.")
+   
+print("____________E_D_I_T_O_R____________")
+print("CAUTION: Add changes will be made to the database you chose above.")
+print("Available options:\n1) add entry, x) close editor")
+ech = input("Enter your choice: ").lower().strip()
+while ech != "x":
+    if ech == "1":
+        journals_data = read_journals(jpath)
+        add_entry(jpath)
     else:
-        journals_data = read_journals(ch)
-        print(f"Loaded the data from {ch}. Printing it below:")
-        print(journals_data)
+        print("Invalid choice.")
+    ech = input("Enter your next choice: ").lower().strip()
+
+print("____________C_O_M_P_I_L_E_R____________")
+name = input("Enter below the name in which you want your files saved. If the files are already existing then supply the same name.\n").strip()
+print_journals(name, jpath)
+    
+    
